0fff80409b9ad35e9dee850687adf1447fa08706
Mac Signer <macsigner@gmx.ch>
2025-01-25 21:39:57 +0100

    feat: move table to custom file

---
+++ src/js/includes/modules/LookupTable.js
@@ -0,0 +1,55 @@
+
+import { delegate } from '../tools.js';
+import { data } from '../data.js';
+
+class LookupTable {
+    constructor(el, options = {
+        data: data.cashflowAnalysis,
+        title: 'Cashflow-Analyse',
+    }) {
+        const data = options.data;
+        const dependencies = Array.from(new Set(data.flatMap(d => d.dependencies))).sort();
+
+        let header = dependencies.map((dRow, dKey) => `<th data-col="${dKey}">${dRow}</th>`).join('');
+
+        let rows = dependencies.map((dRow, keyRow) => {
+            const cells = dependencies.map((dCol, keyCol) => {
+                const possibleResults = data.filter(item => {
+                    return dRow !== dCol && item.dependencies.includes(dRow) && item.dependencies.includes(dCol);
+                }).map(item => {
+                    return `<div class="result">${item.name}<div class="formula">${item.formula}</div></div>`;
+                }).join('');
+
+
+                return `<td data-col="${keyCol}" data-row="${keyRow}">${possibleResults}</td>`;
+            }).join('');
+
+            return `<tr><th data-row="${keyRow}">${dRow}</th>${cells}</tr>`;
+        }).join('');
+
+        let table = `
+            <h1>${options.title}</h1>
+            <table style="--cols: ${dependencies.length + 1}">
+                <tr><th></th>${header}</tr>
+                ${rows}
+            </table>`;
+
+        el.innerHTML = table;
+
+        el.addEventListener('click', delegate('[data-row]', e => {
+            const i = e.target.closest('[data-row]').dataset.row;
+
+            el.querySelectorAll('.active-row').forEach(el => el.classList.remove('active-row'));
+            el.querySelectorAll(`[data-row="${i}"]`).forEach(el => el.classList.add('active-row'));
+        }))
+
+        el.addEventListener('click', delegate('[data-col]', e => {
+            const i = e.target.closest('[data-col]').dataset.col;
+
+            el.querySelectorAll('.active-col').forEach(el => el.classList.remove('active-col'));
+            el.querySelectorAll(`[data-col="${i}"]`).forEach(el => el.classList.add('active-col'));
+        }))
+    }
+}
+
+export default LookupTable;
--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,6 @@
 import '../style.scss'
 import { data } from './includes/data.js';
-import { delegate } from './includes/tools.js';
 
-const dependencies = Array.from(new Set(data.cashflowAnalysis.flatMap(d => d.dependencies))).sort();
+import LookupTable from './includes/modules/LookupTable.js';
 
-let header = dependencies.map((dRow, dKey) => `<th data-col="${dKey}">${dRow}</th>`).join('');
-
-let rows = dependencies.map((dRow, keyRow) => {
-    const cells = dependencies.map((dCol, keyCol) => {
-        const possibleResults = data.cashflowAnalysis.filter(item => {
-            return dRow !== dCol && item.dependencies.includes(dRow) && item.dependencies.includes(dCol);
-        }).map(item => {
-            return `<div class="result">${item.name}<div class="formula">${item.formula}</div></div>`;
-        }).join('');
-
-
-        return `<td data-col="${keyCol}" data-row="${keyRow}">${possibleResults}</td>`;
-    }).join('');
-
-    return `<tr><th data-row="${keyRow}">${dRow}</th>${cells}</tr>`;
-}).join('');
-
-let table = `
-<table style="--cols: ${dependencies.length + 1}">
-    <tr><th></th>${header}</tr>
-    ${rows}
-</table>
-`;
-
-document.querySelector('#app').innerHTML = table;
-
-document.addEventListener('click', delegate('[data-row]', e => {
-    const i = e.target.closest('[data-row]').dataset.row;
-
-    document.querySelectorAll('.active-row').forEach(el => el.classList.remove('active-row'));
-    document.querySelectorAll(`[data-row="${i}"]`).forEach(el => el.classList.add('active-row'));
-}))
-
-document.addEventListener('click', delegate('[data-col]', e => {
-    const i = e.target.closest('[data-col]').dataset.col;
-
-    document.querySelectorAll('.active-col').forEach(el => el.classList.remove('active-col'));
-    document.querySelectorAll(`[data-col="${i}"]`).forEach(el => el.classList.add('active-col'));
-}))
+new LookupTable(document.querySelector('#app'));
