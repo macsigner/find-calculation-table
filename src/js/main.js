89cb3e55d24d5324a748803075698fef284316eb
Mac Signer <macsigner@gmx.ch>
2025-01-29 20:33:21 +0100

    feat: add header toggle form

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,20 @@ import App from './includes/modules/App.js';
 import './includes/modules/DataEntry.js';
 
 document.querySelectorAll('#app').forEach(el => new App(el));
+
+let prevClasses;
+document.querySelector('[data-global-settings]')?.addEventListener('input', e => {
+    const formData = new FormData(e.target.form);
+    let classes = [];
+
+    for (let entry of formData.entries()) {
+        classes.push(`${entry[0]}-${entry[1]}`)
+    }
+
+    if (prevClasses) {
+        prevClasses.forEach(cssClass => document.documentElement.classList.remove(cssClass));
+    }
+
+    classes.forEach(cssClass => document.documentElement.classList.add(cssClass));
+    prevClasses = classes;
+});
